7d768cbfd5a38e8223c90cbfd2bda9c5af9fa5fe
[Lewis Kori] <[korilewis958@gmail.com]>
2024-10-22 10:21:15 +0300

    first example code

---
+++ Example_1/Example_1.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "pin_configurations.h"
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+void setup() {
+  Serial.begin(115200);
+  // Setting up custom I2C pins
+  Wire.begin(I2C_SDA, I2C_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+}
+void loop() {
+  display.clearDisplay();  // Clear display
+  display.setTextSize(1);  // Set text size
+  display.setCursor(0, 10);  // Set cursor position
+  display.println("Hello, world!");  // Print "Hello, world!" on the display
+  display.display();  // Display text
+  delay(1000);  // Delay for 1 second
+}
